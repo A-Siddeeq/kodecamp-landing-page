f6ef6871585931ac2970589e17d479c199ee588a
A-siddeeq <abubakaradamu1163@gmail.com>
2024-06-10 22:40:56 +0100

    adjustments in mainpage

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,32 +1,116 @@
 import React from 'react';
 import logo from '../assets/logo.svg'
 import { Link } from 'react-router-dom';
+import { useState } from "react"; 
 
-const Nav = () => {
+export default function Nav() {
+  const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
+
   return (
-    <div className='flex items-center justify-around p-4'>
-        <div>
-            <img src={logo} alt="logo" height={42} width={69}/>
-        </div>
-   
-        <div>
-            <ul className='flex gap-x-4 font-normal'>
-                <li className='hover:border-t-2 hover:border-b-2'>
-                    <Link to="/">Main</Link>
-                </li>
-                <li className='hover:border-t-2 hover:border-b-2'>
-                    <Link to="/gallery">Gallery</Link>
-                </li>
-                <li className='hover:border-t-2 hover:border-b-2'>
-                    <Link to="/projects">Projects</Link>
-                </li>
-                <li>Certifications</li>
-                <li>Contact</li>
+    <div className="flex items-center justify-between border-gray-400 py-8">
+      <a href="/">
+       <img src={logo} alt="logo" height={42} width={69}/>
+      </a>
+      <nav className='py-4 px-6'>
+        <section className="MOBILE-MENU flex lg:hidden">
+          <div
+            className="HAMBURGER-ICON space-y-2"
+            onClick={() => setIsNavOpen((prev) => !prev)} // toggle isNavOpen state on click
+          >
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+          </div>
+
+          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+            <img src="{logo}" alt="" width={100} height={100}/>
+            <div
+              className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
+            >
+              <svg
+                className="h-8 w-8 text-gray-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </div>
+            <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
+            <li className='hover:border-t-2 hover:border-b-2'>
+            <Link to="/">Main</Link>
+        </li>
+        <li className='hover:border-t-2 hover:border-b-2'>
+            <Link to="/gallery">Gallery</Link>
+        </li>
+        <li className='hover:border-t-2 hover:border-b-2'>
+            <Link to="/projects">Projects</Link>
+        </li>
+        <li>Certifications</li>
+        <li>Contact</li>
             </ul>
-        </div>
- 
+          </div>
+        </section>
+
+        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
+        <li className='hover:border-t-2 hover:border-b-2'>
+            <Link to="/">Main</Link>
+        </li>
+        <li className='hover:border-t-2 hover:border-b-2'>
+            <Link to="/gallery">Gallery</Link>
+        </li>
+        <li className='hover:border-t-2 hover:border-b-2'>
+            <Link to="/projects">Projects</Link>
+        </li>
+        <li>Certifications</li>
+        <li>Contact</li>
+        </ul>
+        </nav>
+      <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: max-content;
+        top: 5;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+    `}</style>
     </div>
-  )
+  );
 }
 
-export default Nav
+{/* <div className='flex items-center justify-around p-4'>
+<div>
+    <img src={logo} alt="logo" height={42} width={69}/>
+</div>
+
+<div>
+    <ul className='flex gap-x-4 font-normal'>
+        <li className='hover:border-t-2 hover:border-b-2'>
+            <Link to="/">Main</Link>
+        </li>
+        <li className='hover:border-t-2 hover:border-b-2'>
+            <Link to="/gallery">Gallery</Link>
+        </li>
+        <li className='hover:border-t-2 hover:border-b-2'>
+            <Link to="/projects">Projects</Link>
+        </li>
+        <li>Certifications</li>
+        <li>Contact</li>
+    </ul>
+</div> */}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -7,6 +7,7 @@ import image2 from '../assets/image2.png'
 import image3 from '../assets/image3.png'
 import image4 from '../assets/image4.png'
 import image5 from '../assets/image5.png'
+import image5over from '../assets/image5over.png'
 import image6 from '../assets/image6.png'
 import image7 from '../assets/image7.png'
 import image8 from '../assets/image8.png'
@@ -20,7 +21,7 @@ const MainPage = () => {
     <div>
       <main className='px-6'>
         {/*PROJECT LORUM SECTION */}
-        <div className='flex flex-col md:flex-row py-12'>
+        <div className='flex flex-col items-center md:flex-row py-12'>
          <div className='flex flex-col items-center justify-center md:w-1/2'>
           <h1 className='font-extralight text-[64px] leading-[64px] text-[#BDBDBD]'>PROJECT</h1>
           <h1 className='font-bold text-[64px] leading-[64px]'>Lorum</h1>
@@ -28,32 +29,45 @@ const MainPage = () => {
             <button className='w-[53px] h-[53px] border-2 mx-2 flex justify-center'>
                 <img src={back} alt="" />
             </button>
-            <button className='w-[53px] h-[53px] border-2 mx-2 flex justify-center'>
+            <button className='w-[53px] h-[53px] border-2 mx-2 flex justify-center bg-[#F9F9F9]'>
                 <img src={next} alt="" />
             </button>
         </div>
          </div>
-         <div className='flex justify-center items-center w-3/4 md:w-1/2'>
+         <div className='relative w-3/4 md:w-1/2'>
           <img src={image1} alt="" className='md:w-["770px"] md:h-["830px"]'/>
+          <button className='flex justify-evenly items-center text-[12px] tracking-widest absolute w-[200px] h-[71px] bottom-0 bg-white'>VIEW PROJECTS
+          <svg width="20" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.9353 11.5C3.9353 11.2239 4.15916 11 4.4353 11L20.4353 11C20.7114 11 20.9353 11.2239 20.9353 11.5C20.9353 11.7761 20.7114 12 20.4353 12L4.4353 12C4.15916 12 3.9353 11.7761 3.9353 11.5Z" fill="#333333"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M16.5817 7.64645C16.777 7.45118 17.0936 7.45118 17.2889 7.64645L21.1424 11.5L17.2889 15.3536C17.0936 15.5488 16.777 15.5488 16.5817 15.3536C16.3865 15.1583 16.3865 14.8417 16.5817 14.6464L19.7282 11.5L16.5817 8.35355C16.3865 8.15829 16.3865 7.84171 16.5817 7.64645Z" fill="#333333"/>
+          </svg>
+
+          </button>
          </div>
         </div>
 
         {/*ABOUT SECTION */}
-        <div className='flex flex-col-reverse md:flex-row py-12'>
+        <div className='flex flex-col-reverse md:flex-row items-center py-12 bg-[#F9F9F9]'>
           <div className='grid grid-cols-3 md:grid-cols-2 gap-4 py-4 justify-center items-center w-3/4 md:w-1/2'>
           <img src={image2} alt="" className='md:w-["770px"] md:h-["830px"] w-full h-full object-cover'/>
           <img src={image3} alt="" className='md:w-["770px"] md:h-["830px"] w-full h-full object-cover'/>
           <img src={image4} alt="" className='md:w-["770px"] md:h-["830px"] w-full h-full object-cover'/>
          </div>
-         <div className='flex flex-col items-center justify-center md:w-1/2'>
-          <h1 className='font-extralight text-[64px] leading-[64px] py-6 text-[#BDBDBD]'>ABOUT</h1>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         <div className='flex flex-col items-center justify-start md:gap-10 md:w-1/2'>
+          <h1 className='font-extralight text-[64px] leading-[64px] py-6 text-[#BDBDBD] self-start md:self-center'>About</h1>
+          <p className='max-w-[400px]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
               when an unknown printer took a galley of type and scrambled it to make a 
               type specimen book. It has survived not only five centuries, but also the
              leap into electronic typesetting, 
             remaining essentially unchanged.</p>
-            <button className='w-[200px] h-[71px] bg-slate-400'>Read More...</button>
+            <button className='flex justify-evenly items-center text-[12px] tracking-widest w-[200px] h-[71px] bottom-0 bg-white'>READ MORE
+          <svg width="20" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.9353 11.5C3.9353 11.2239 4.15916 11 4.4353 11L20.4353 11C20.7114 11 20.9353 11.2239 20.9353 11.5C20.9353 11.7761 20.7114 12 20.4353 12L4.4353 12C4.15916 12 3.9353 11.7761 3.9353 11.5Z" fill="#333333"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M16.5817 7.64645C16.777 7.45118 17.0936 7.45118 17.2889 7.64645L21.1424 11.5L17.2889 15.3536C17.0936 15.5488 16.777 15.5488 16.5817 15.3536C16.3865 15.1583 16.3865 14.8417 16.5817 14.6464L19.7282 11.5L16.5817 8.35355C16.3865 8.15829 16.3865 7.84171 16.5817 7.64645Z" fill="#333333"/>
+          </svg>
+
+          </button>
          </div>
         </div>
 
@@ -84,11 +98,30 @@ const MainPage = () => {
             <h1 className='font-extralight text-[64px] leading-[64px] py-6 text-[#BDBDBD]'>Our Projects</h1>
 
             <div className='grid grid-cols-2 md:grid-cols-6 gap-4 items-center'>
-              <img src={image5} alt="" className='md:col-span-3 w-full h-full object-cover'/>
-              <img src={image6} alt="" className='md:col-span-3 w-full h-full object-cover'/>
-              <img src={image7} alt="" className='md:col-span-1 w-full h-full object-cover'/>
-              <img src={image8} alt="" className='md:col-span-3 w-full h-full object-cover'/>
-              <img src={image9} alt="" className='md:col-span-2 w-full h-full object-cover'/>
+              <div className='relative md:col-span-3 w-full h-full '>
+              <img src={image5} alt="" className='object-cover'/>
+              <img src={image5over} alt="" className='absolute top-0'/>
+              <h1 className='absolute top-0 text-white'>Sample Project</h1>
+              <button className='abolute top-0 text-[12px] tracking-widest w-[200px] h-[71px] bg-white'>READ MORE
+          <svg width="20" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.9353 11.5C3.9353 11.2239 4.15916 11 4.4353 11L20.4353 11C20.7114 11 20.9353 11.2239 20.9353 11.5C20.9353 11.7761 20.7114 12 20.4353 12L4.4353 12C4.15916 12 3.9353 11.7761 3.9353 11.5Z" fill="#333333"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M16.5817 7.64645C16.777 7.45118 17.0936 7.45118 17.2889 7.64645L21.1424 11.5L17.2889 15.3536C17.0936 15.5488 16.777 15.5488 16.5817 15.3536C16.3865 15.1583 16.3865 14.8417 16.5817 14.6464L19.7282 11.5L16.5817 8.35355C16.3865 8.15829 16.3865 7.84171 16.5817 7.64645Z" fill="#333333"/>
+          </svg>
+
+          </button>
+              </div>
+              <div className='md:col-span-3 w-full h-full '>
+              <img src={image6} alt="" className='object-cover'/>
+              </div>
+              <div className='md:col-span-1 w-full h-full '>
+              <img src={image7} alt="" className='object-cover'/>
+              </div>
+              <div className='md:col-span-3 w-full h-full '>
+              <img src={image8} alt="" className='object-cover'/>
+              </div>
+              <div className='md:col-span-2 w-full h-full '>
+              <img src={image9} alt="" className='object-cover'/>
+              </div>
             </div>
           </div>
           <button className='w-[200px] h-[71px] text-white bg-[#2C2C2C] m-2'>ALL PROJECTS</button>
@@ -114,8 +147,8 @@ const MainPage = () => {
              </div>
             </div>
 
-          </div>
-            <button className='w-[200px] h-[71px] text-white bg-[#2C2C2C] m-4 ml-32 md:ml-10 self-center lg:self-auto'>SEND EMAIL</button>
+          </div >
+            <button className='w-[200px] h-[71px] text-white bg-[#2C2C2C] m-4 ml-32 md:ml-10 justify-self-center lg:self-auto'>SEND EMAIL</button>
         </div>
        </main>
         
